7e00ae18049d5c27dbfb58e51ff4017d848e4f72
Pouriya <pouriya.jamshidi@gmail.com>
2021-05-19 18:02:17 +0200

    Modify root vdom handling

---
+++ README.md
@@ -0,0 +1,53 @@
+# Fortigate SSL VPN
+
+Python script that is written to display your connected SSL VPN users in an systematized manner.
+
+## Features
+
+* Query multiple firewalls (specified in yaml file)
+* Helps you find users who are possibly sharing their account credentials
+* Be aware of your IP assignments and address pool depletion
+* Possibly find SSL VPN daemon issues of FortiOS
+* Cross-platform. Runs on Windows, Linux and Mac OS.
+
+## Requirements
+
+Netmiko is needed to run this script. You can install it using below command.
+
+```bash
+pip3 install netmiko
+```
+
+## Usage
+
+Populate the `firewalls.yaml` according to your environment:
+
+* name: Firewall name
+* mgmt_ip: management IP address
+* port: SSH port
+* vdom: Vdom name, in case you are not using any, set it as `root`
+* ip_pool: IP address pool size for VPN users
+
+For FortiOS versions **below 6.4**:
+
+```bash
+python3 pysslvpnmonitor_below_6.4_Version.py
+```
+
+For FortiOS versions **6.4 and above**:
+
+```sh
+python3 pysslvpnmonitor.py
+```
+
+## Tested on
+
+Windows and Debian derivatives.
+
+## Contributing
+
+Pull requests are welcome.
+
+## License
+
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
---
+++ pysslvpnmonitor.py
@@ -0,0 +1,307 @@
+#!/usr/bin/env python3
+
+import signal
+import time
+from os import getenv
+from os.path import isfile
+
+from netmiko import ConnectHandler
+from netmiko.ssh_exception import (AuthenticationException,
+                                   NetMikoTimeoutException)
+from paramiko.ssh_exception import SSHException
+from yaml import safe_load
+
+
+class txtcolor:
+    """
+    This class helps with terminal output colorization
+    """
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    CYAN = '\033[1;36m'
+    ORANGE = '\033[43m'
+    PURPLE = '\033[45m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+class FortigateSSLVPN:
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.SignalHandler)
+        self.filename_list = []
+
+    def SignalHandler(self, frame, signal):
+        print("\nCaught SIGINT\nExiting...")
+        exit(1)
+
+    def GetTime(self):
+        '''
+        Get time to append to files.
+        '''
+        return time.strftime("%Y%m%d%H%M")
+
+    def WriteFile(self, filename, inputcontent):
+        '''
+        Write gathered SSH output to file.
+        '''
+
+        self.timestamp = self.GetTime()
+        self.filename = str(filename + "_" + self.timestamp + ".log")
+
+        print(f"[+] Saving to {self.filename}")
+
+        with open(self.filename, 'w+') as file:
+            file.write(inputcontent)
+
+        self.filename_list.append(self.filename)
+
+        print(f"[+] {self.filename} created and populated.")
+
+    def ReadFile(self, filename):
+        '''
+        Read and return file content
+        for further processing.
+        '''
+
+        print(f"[+] Reading {filename}")
+
+        with open(filename, "r") as file:
+            return file.readlines()
+
+    def GenerateSSLVPNDic(self, content):
+        '''
+        Generate SSL-VPN user dictionary
+        for further processing.
+        '''
+        printer = False
+        usrdic = {}
+        self.IP_pool = []
+
+        for line in content:
+
+            if line.strip() == "SSL VPN sessions:":
+                printer = True
+                continue
+            if "Index" in line:
+                continue
+            if printer == True:
+                if len(line) <= 4:
+                    continue
+
+                line = line.rstrip().split(" ")
+                VPNIPAddr = line[-1].strip()
+                username = line[3]
+                PubIPAddr = line[-6].split("\t")[1]
+                cnt = 0
+
+                self.IP_pool.append(VPNIPAddr)
+
+                if username in usrdic:
+                    usrdic[username][2] += 1
+                    usrdic[username].append([VPNIPAddr, PubIPAddr])
+                    continue
+
+                usrdic[username] = [VPNIPAddr, PubIPAddr, cnt + 1]
+
+        return sorted(usrdic.items())
+
+    def DisplaySSLUsers(self):
+        '''
+        Display currently connected SSL-VPN
+        Users and create duplicate list.
+        '''
+
+        filenamelist = self.filename_list
+
+        for filename in filenamelist:
+            content = self.ReadFile(filename)
+            sorted_usrdic = self.GenerateSSLVPNDic(content)
+            self.dup_users = []
+            lncounter = 1
+
+            fwname = filename.split('-')[0]
+
+            print(f"[+] Currently Connected SSL-VPN Users in {fwname}:")
+            print("=" * 85)
+            print("# Username\t\t\t VPN IP\t\t\tPublic IP\t\tSeen")
+            print("=" * 85)
+
+            for i in sorted_usrdic:
+                userlen = len(i[0])
+                username = i[0]
+                VPNIP = i[1][0].strip()
+                PubIP = i[1][1].strip()
+                Seen = i[1][2]
+                print(f"{lncounter}) {username}" + " " * (30 - userlen) + f"{VPNIP}" + "\t\t"
+                      + f"{PubIP}" + f"\t\t{Seen}")
+                print("-" * 85)
+                lncounter += 1
+
+                if Seen >= 2:
+                    self.dup_users.append(i)
+
+            self.total_users = len(sorted_usrdic)
+
+            if len(self.dup_users) > 0:
+                self.DisplayDuplicateSSLUsers(fwname)
+
+            self.SSLVPNSummary(fwname, self.total_users, self.IP_pool)
+
+    def DisplayDuplicateSSLUsers(self, fwname):
+        '''
+        Display users who have connected
+        more than once.
+        '''
+        print("\n\n")
+        print(f"[+] Duplicate SSL-VPN Users in {fwname}")
+        print("=" * 85)
+        print("# Username\t\t\t VPN IP\t\t\tPublic IP")
+        print("=" * 85)
+        lncounter = 1
+
+        dup_users = self.dup_users
+        dup_users.sort(key=lambda x: x[1][2], reverse=True)
+
+        for j in dup_users:
+            count = j[1][2]
+            VPNIP = j[1][0].strip()
+            PubIP = j[1][1].strip()
+            username = j[0]
+            userlen = len(j[0])
+            Seen = j[1][2]
+
+            if count > 1:
+                print(f"{lncounter}) {username}" + " " *
+                      (30 - userlen) + f"{VPNIP}" + "\t\t" + f"{PubIP}")
+                for k in j[1]:
+                    if isinstance(k, int):
+                        pass
+                    elif len(k) == 2:
+                        VPNIP = k[0].strip()
+                        PubIP = k[1].strip()
+                        print(f"{lncounter}) {username}" + " " * (30 -
+                                                                  userlen) + f"{VPNIP}" + "\t\t" + f"{PubIP}")
+                print(f"[*] Seen User for {Seen} times!")
+                print("-" * 85)
+                lncounter += 1
+
+    def SSLVPNSummary(self, fwname, total_users, IP_pool):
+
+        print(
+            f"\n[+] Total number of connected users in {fwname}: {total_users}")
+        print(
+            f"[+] Remaining IP addresses: {int(self.available_IP_pool) - len(IP_pool)}\n")
+
+    def handle_yaml_file(self, yamlfile):
+        '''
+        Open YAML file to get firewall info
+        '''
+
+        if isfile(yamlfile):
+            with open(yamlfile, "r") as fwfile:
+                firewalls = safe_load(fwfile)
+            return firewalls
+        else:
+            print("Could not read firewalls.yml file")
+            exit(1)
+
+    def fetch_firewall_info(self, user, passwd):
+        '''
+        get firewall name, IP and port
+        '''
+
+        firewalls = self.handle_yaml_file("firewalls.yml")
+
+        for fw in firewalls["firewalls"]:
+            fwname = fw["name"]
+            fwip = fw["mgmt_ip"]
+            fwport = fw["port"]
+            fwvdom = fw["vdom"]
+            self.available_IP_pool = fw["ip_pool"]
+            self.SSHConnect(fwname, fwip, fwport, fwvdom, user, passwd)
+
+    def SSHConnect(self, name, ip, port, fwvdom, user, passwd):
+        '''
+        Connect to Firewall and generate log file to process.
+        '''
+
+        if passwd == None:
+            firewall = {
+                'device_type': 'fortinet',
+                'ip': ip,
+                'port': port,
+                'username': user,
+                'use_keys': True,
+                'key_file': '/root/.ssh/id_rsa',
+            }
+        elif passwd:
+            firewall = {
+                'device_type': 'fortinet',
+                'ip': ip,
+                'port': port,
+                'username': user,
+                'password': passwd,
+            }
+
+        print(f"[+] Initiating connection to {name} - {ip}")
+
+        try:
+            net_connect = ConnectHandler(**firewall)
+        except Exception as e:
+            print(f"[-] Failed to connect to {name} using {ip}")
+            print(e)
+            return
+
+        print(f"[+] Connected to {name}")
+        print("[+] Sending commands")
+
+        if fwvdom != "root":
+            net_connect.send_command(
+                "config vdom", expect_string=r"#", delay_factor=1)
+            net_connect.send_command(
+                f"edit {fwvdom}", expect_string=r"#", delay_factor=1)
+            output = net_connect.send_command(
+                "get vpn ssl monitor", expect_string=r"#", delay_factor=1)
+        else:
+            output = net_connect.send_command(
+                "get vpn ssl monitor", expect_string=r"#", delay_factor=1)
+
+        print("[+] Output gathered")
+
+        filename = f"{name}-SSLVPN"
+        self.WriteFile(filename, output)
+
+        net_connect.disconnect()
+
+        print(f"[+] Disconnected from {name}.")
+
+    def fetch_credentials(self):
+
+        print("[+] Fetching credentials")
+
+        user = getenv("FORTIUSER")
+        passwd = getenv("FORTIPASS")
+
+        if user == None or passwd == None:
+            print("Could not fetch credentials from ENV")
+            user = input("Username: ")
+            passwd = input("Password: ")
+
+        return user, passwd
+
+    def Run(self):
+
+        user, passwd = self.fetch_credentials()
+
+        self.fetch_firewall_info(user, passwd)
+
+        Forti.DisplaySSLUsers()
+
+
+if __name__ == "__main__":
+    Forti = FortigateSSLVPN()
+    Forti.Run()
